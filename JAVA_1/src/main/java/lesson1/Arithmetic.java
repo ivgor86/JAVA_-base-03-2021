d8c7e889e04e0d2363daa13ff4af67ff74a785cb
ivgor86 <80530673+ivgor86@users.noreply.github.com>
2021-03-15 17:21:49 +0300

    Delete JAVA_1/src/main/java/lesson1 directory

--- JAVA_1/src/main/java/lesson1/Arithmetic.java
+++
@@ -1,9 +0,0 @@
-package lesson1;
-
-public class Arithmetic {
-    public static void main(String[] args) {
-        // + - * %
-        int a = 5, b = 3, c= 7, d = 8;
-        System.out.println((float) a * (b + (c / d) ) );
-    }
-}
